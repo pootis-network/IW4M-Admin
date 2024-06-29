451b25ffb26a7a06f796f60aa6c9839f94f6d9cf
RaidMax <raidmax@live.com>
2024-06-29 16:54:06 -0500

    simplify build script for local builds

---
+++ Application/BuildScripts/PreBuild.ps1
@@ -0,0 +1,59 @@
+﻿param ( [string]$SolutionDir, [string]$OutputDir )
+
+if (-not (Test-Path "$SolutionDir/WebfrontCore/wwwroot/font")) {
+    Write-Output "restoring web dependencies"
+    dotnet tool install Microsoft.Web.LibraryManager.Cli --global
+    Set-Location "$SolutionDir/WebfrontCore"
+    libman restore
+    Set-Location $SolutionDir
+    Copy-Item -Recurse -Force -Path "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/fonts" "$SolutionDir/WebfrontCore/wwwroot/font"
+}
+
+if (-not (Test-Path "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css/open-iconic-bootstrap-override.scss")) {
+    Write-Output "load external resources"
+    New-Item -ItemType Directory -Force -Path "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/iconic/open-iconic/master/font/css/open-iconic-bootstrap.scss" -OutFile "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css/open-iconic-bootstrap-override.scss"
+    (Get-Content "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css/open-iconic-bootstrap-override.scss") -replace '../fonts/', '/font/' | Set-Content "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css/open-iconic-bootstrap-override.scss"
+}
+
+Write-Output "compiling scss files"
+
+dotnet tool install Excubo.WebCompiler --global
+webcompiler -r "$SolutionDir/WebfrontCore/wwwroot/css/src" -o WebfrontCore/wwwroot/css/ -m disable -z disable
+webcompiler "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css/open-iconic-bootstrap-override.scss" -o "$SolutionDir/WebfrontCore/wwwroot/css/" -m disable -z disable
+
+if (-not (Test-Path "$SolutionDir/bundle/dotnet-bundle.dll")) {
+    New-Item -ItemType Directory -Force -Path "$SolutionDir/bundle"
+    Write-Output "getting dotnet bundle"
+    Invoke-WebRequest -Uri "https://raidmax.org/IW4MAdmin/res/dotnet-bundle.zip" -OutFile "$SolutionDir/bundle/dotnet-bundle.zip"
+    Write-Output "unzipping download"
+    Expand-Archive -Path "$SolutionDir/bundle/dotnet-bundle.zip" -DestinationPath "$SolutionDir/bundle" -Force
+}
+
+Write-Output "executing dotnet-bundle"
+Set-Location "$SolutionDir/bundle"
+dotnet "dotnet-bundle.dll" clean "$SolutionDir/WebfrontCore/bundleconfig.json"
+dotnet "dotnet-bundle.dll" "$SolutionDir/WebfrontCore/bundleconfig.json"
+Set-Location $SolutionDir
+
+New-Item -ItemType Directory -Force -Path "$SolutionDir/BUILD/Plugins"
+Write-Output "building plugins"
+Set-Location "$SolutionDir/Plugins"
+Get-ChildItem -Recurse -Filter *.csproj | ForEach-Object { dotnet publish $_.FullName -o "$SolutionDir/BUILD/Plugins" --no-restore }
+Set-Location $SolutionDir
+
+if (-not (Test-Path "$OutputDir/Localization")) {
+    Write-Output "downloading translations"
+    New-Item -ItemType Directory -Force -Path "$OutputDir/Localization"
+    $localizations = @("en-US", "ru-RU", "es-EC", "pt-BR", "de-DE")
+    foreach ($localization in $localizations) {
+        $url = "https://master.iw4.zip/localization/$localization"
+        $filePath = "$OutputDir/Localization/IW4MAdmin.$localization.json"
+        Invoke-WebRequest -Uri $url -OutFile $filePath -UseBasicParsing
+    }
+}
+
+Write-Output "copying plugins to build dir"
+New-Item -ItemType Directory -Force -Path "$OutputDir/Plugins"
+Copy-Item -Recurse -Force -Path "$SolutionDir/BUILD/Plugins/*.dll" -Destination "$OutputDir/Plugins/"
+Copy-Item -Recurse -Force -Path "$SolutionDir/Plugins/ScriptPlugins/*.js" -Destination "$OutputDir/Plugins/"
---
+++ Application/BuildScripts/PreBuild.sh
@@ -0,0 +1,63 @@
+﻿#!/bin/bash
+
+SolutionDir="$1"
+OutputDir="$2"
+export PATH="$PATH:~/.dotnet/tools"
+
+if [ ! -d "$SolutionDir/WebfrontCore/wwwroot/lib/font" ]; then
+    echo restoring web dependencies
+    dotnet tool install Microsoft.Web.LibraryManager.Cli --global
+    cd "$SolutionDir/WebfrontCore" || exit
+    libman restore
+    cd "$SolutionDir" || exit
+    cp -r "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/fonts" "$SolutionDir/WebfrontCore/wwwroot/font"
+fi
+
+if [ ! -f "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css/open-iconic-bootstrap-override.scss" ]; then
+    echo load external resources
+    mkdir -p "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css"
+    curl -o "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css/open-iconic-bootstrap-override.scss" https://raw.githubusercontent.com/iconic/open-iconic/master/font/css/open-iconic-bootstrap.scss
+    sed -i 's#../fonts/#/font/#g' "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css/open-iconic-bootstrap-override.scss"
+fi
+
+echo compiling scss files
+
+dotnet tool install Excubo.WebCompiler --global
+webcompiler -r "$SolutionDir/WebfrontCore/wwwroot/css/src" -o "$SolutionDir/WebfrontCore/wwwroot/css/" -m disable -z disable
+webcompiler "$SolutionDir/WebfrontCore/wwwroot/lib/open-iconic/font/css/open-iconic-bootstrap-override.scss" -o "$SolutionDir/WebfrontCore/wwwroot/css/" -m disable -z disable
+
+if [ ! -f "$SolutionDir/bundle/dotnet-bundle.dll" ]; then
+    mkdir -p "$SolutionDir/bundle"
+    echo getting dotnet bundle
+    curl -o "$SolutionDir/bundle/dotnet-bundle.zip" https://raidmax.org/IW4MAdmin/res/dotnet-bundle.zip
+    echo unzipping download
+    unzip "$SolutionDir/bundle/dotnet-bundle.zip" -d "$SolutionDir/bundle"
+fi
+echo executing dotnet-bundle
+cd "$SolutionDir/bundle" || exit
+dotnet dotnet-bundle.dll clean "$SolutionDir/WebfrontCore/bundleconfig.json"
+dotnet dotnet-bundle.dll "$SolutionDir/WebfrontCore/bundleconfig.json"
+cd "$SolutionDir" || exit
+
+mkdir -p "$SolutionDir/BUILD/Plugins"
+echo building plugins
+cd "$SolutionDir/Plugins" || exit 
+find . -name "*.csproj" -print0 | xargs -0 -I {} dotnet publish {} -o "$SolutionDir/BUILD/Plugins" --no-restore
+cd "$SolutionDir" || exit
+
+if [ ! -d "$OutputDir/Localization" ]; then
+    echo downloading translations
+    mkdir -p "$OutputDir/Localization"
+    localizations=("en-US" "ru-RU" "es-EC" "pt-BR" "de-DE")
+    for localization in "${localizations[@]}"
+    do
+        url="https://master.iw4.zip/localization/$localization"
+        filePath="$OutputDir/Localization/IW4MAdmin.$localization.json"
+        curl -s "$url" -o "$filePath"
+    done
+fi
+
+echo copying plugins to buld dir
+mkdir -p "$OutputDir/Plugins"
+cp -r "$SolutionDir/BUILD/Plugins/*.dll" "$OutputDir/Plugins/"
+cp -r "$SolutionDir/Plugins/ScriptPlugins/*.js" "$OutputDir/Plugins/"

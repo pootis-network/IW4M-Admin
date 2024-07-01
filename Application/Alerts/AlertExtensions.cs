e94a0ae6911e9971955e24aa1da866cb79a2bf6d
Amos <amos2580@hotmail.co.uk>
2024-07-01 18:32:53 +0100

    Fix Commit - XML Comments

--- Application/Alerts/AlertExtensions.cs
+++ Application/Alerts/AlertExtensions.cs
@@ -5,8 +5,17 @@ using SharedLibraryCore.Database.Models;
 
 namespace IW4MAdmin.Application.Alerts;
 
+/// <summary>
+/// extension method helper class to allow building of alerts
+/// </summary>
 public static class AlertExtensions
 {
+    /// <summary>
+    /// builds basic alert for user with provided category
+    /// </summary>
+    /// <param name="client">client to build the alert for</param>
+    /// <param name="type">alert category</param>
+    /// <returns></returns>
     public static Alert.AlertState BuildAlert(this EFClient client, Alert.AlertCategory? type = null)
     {
         return new Alert.AlertState
@@ -16,36 +25,72 @@ public static class AlertExtensions
         };
     }
 
+    /// <summary>
+    /// sets the category for an existing alert
+    /// </summary>
+    /// <param name="state">existing alert</param>
+    /// <param name="category">new category</param>
+    /// <returns></returns>
     public static Alert.AlertState WithCategory(this Alert.AlertState state, Alert.AlertCategory category)
     {
         state.Category = category;
         return state;
     }
 
+    /// <summary>
+    /// sets the alert type for an existing alert
+    /// </summary>
+    /// <param name="state">existing alert</param>
+    /// <param name="type">new type</param>
+    /// <returns></returns>
     public static Alert.AlertState OfType(this Alert.AlertState state, string type)
     {
         state.Type = type;
         return state;
     }
 
+    /// <summary>
+    /// sets the message for an existing alert
+    /// </summary>
+    /// <param name="state">existing alert</param>
+    /// <param name="message">new message</param>
+    /// <returns></returns>
     public static Alert.AlertState WithMessage(this Alert.AlertState state, string message)
     {
         state.Message = message;
         return state;
     }
 
+    /// <summary>
+    /// sets the expiration duration for an existing alert
+    /// </summary>
+    /// <param name="state">existing alert</param>
+    /// <param name="expiration">duration before expiration</param>
+    /// <returns></returns>
     public static Alert.AlertState ExpiresIn(this Alert.AlertState state, TimeSpan expiration)
     {
         state.ExpiresAt = DateTime.Now.Add(expiration);
         return state;
     }
-    
+
+    /// <summary>
+    /// sets the source for an existing alert
+    /// </summary>
+    /// <param name="state">existing alert</param>
+    /// <param name="source">new source</param>
+    /// <returns></returns>
     public static Alert.AlertState FromSource(this Alert.AlertState state, string source)
     {
         state.Source = source;
         return state;
     }
 
+    /// <summary>
+    /// sets the alert source to the provided client
+    /// </summary>
+    /// <param name="state">existing alert</param>
+    /// <param name="client">new client</param>
+    /// <returns></returns>
     public static Alert.AlertState FromClient(this Alert.AlertState state, EFClient client)
     {
         state.Source = client.Name.StripColors();
